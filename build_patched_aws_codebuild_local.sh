f0559f08f8eea80ac66baec6775a9a3657043f43
Dave Bonner <bonnerd@media.mit.edu>
2018-09-10 12:50:20 -0400

    it helps if local_build.sh is executable

--- build_patched_aws_codebuild_local.sh
+++ build_patched_aws_codebuild_local.sh
@@ -5,5 +5,6 @@ set -e
 virtualenv venv
 pip install -r requirements.txt
 ./extract_file_from_image.py
+chmod a+x ./local_build.sh
 patch < local_build.patch
 docker build -t aws-codebuild-local:bugfix .
